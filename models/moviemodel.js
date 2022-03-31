03cbf2b1b8aaf59c1358c7a452ff47b86b73e579
Tarasish bhattacharya <tarasishbhattacharya06@gmail.com>
2022-03-31 12:51:54 +0530

    changes1

--- models/moviemodel.js
+++ models/moviemodel.js
@@ -1,1 +1,13 @@
 const mongoose=require("mongoose");
+
+const movieSchema=new mongoose.Schema({
+    original_title:{type:String,required:true},
+    poster_path:{type:String,required:true},
+    imdb:{type:Number,required:true},
+    original_lang:{type:String,required:true},
+    overview:{type:String,required:true},
+})
+
+const Movie=mongoose.model("movie",movieSchema);
+
+module.exports=Movie
