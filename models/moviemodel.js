1f6a23e2f8f589be2ceb89ec9462cf552eae12fb
Tarasish bhattacharya <tarasishbhattacharya06@gmail.com>
2022-04-01 12:27:15 +0530

    changes again

--- models/moviemodel.js
+++ models/moviemodel.js
@@ -4,8 +4,8 @@ const movieSchema=new mongoose.Schema({
     original_title:{type:String,required:true},
     poster_path:{type:String,required:true},
     imdb:{type:Number,required:true},
-    original_lang:{type:String,required:true},
-    overview:{type:String,required:true},
+    original_lang:{type:String,required:false},
+    overview:{type:String,required:false},
 })
 
 const Movie=mongoose.model("movie",movieSchema);
