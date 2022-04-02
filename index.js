8b08cacbf560f2e60b25249c7be9ae799a905c92
Rohitraj97 <97448777+Rohitraj97@users.noreply.github.com>
2022-04-02 12:30:35 +0530

    auth done

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
+<<<<<<< Updated upstream
 const express = require("express");
+=======
+const express = require("express")
+ 
+const connect = require("./configs/db")
+ 
+const userController = require("./controller/user.controller")
+// const passport = require("./configs/google-oauth")
+ 
+const app = express()
+const { register, login } = require("./controller/auth.controller")
+app.use(express.json())
+app.use("/users", userController)
+
+
+
+app.post("/register", register)
+
+app.post("/login", login)
+ 
+
+ 
+>>>>>>> Stashed changes
 
 const connect = require("./configs/db");
 
